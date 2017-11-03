1ed1d20b442c2248b2309ecd9d46bbc870f4e742
KireinaHoro <i@jsteward.moe>
2017-11-03 20:25:00 +0800

    add comment

--- sim-mid-term-2/matrix.cc
+++ sim-mid-term-2/matrix.cc
@@ -18,11 +18,17 @@ int main()
             dp[i][i] = m * a[i];
         }
         for(int l = 2; l <= m; l ++)
+            // calculate every interval with length l first
             for(int i = 1; i <= m - 1; i ++)
             {
                 int j = i + l - 1;
                 dp[i][j] = max(dp[i + 1][j] + a[i] * (m - l + 1), dp[i][j - 1] + a[j] * (m - l + 1));
             }
+        /*
+        for(int i = 1; i <= m - 1; i ++)
+            for(int j = i + 1; j <= m; j ++)
+                dp[i][j] = max(dp[i + 1][j] + a[i] * (m - (j - i)), dp[i][j - 1] + a[j] * (m - (j - i)));
+            */
         ans += dp[1][m];
     }
     cout << ans << endl;
